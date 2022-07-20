533826d56571f429fcbcc3cab1e3872073d2f485
khushigoyal018 <108930278+khushigoyal018@users.noreply.github.com>
2022-07-20 17:23:57 +0530

    Add files via upload

---
+++ NoteTaker/src/main/java/com/helper/SessionFactoryProvider.java
@@ -0,0 +1,22 @@
+package com.helper;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryProvider {
+	public static SessionFactory sessionFactory;
+	
+	public static SessionFactory getSessionFactory() {
+		
+		if(sessionFactory==null) {
+			try {
+				Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			sessionFactory=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		}
+		
+		return sessionFactory;
+	}
+}
